d4135b5e7df33be035c1c6e849b35c595ec2268a
Jackson Meyer <jackson.meyer7@gmail.com>
2016-08-12 13:58:11 -0700

    will add pcitures to the branches, js isnt working right now

--- js/scripts.js
+++ js/scripts.js
@@ -11,13 +11,24 @@ $(document).ready(function() {
     $(".csharpdev").hide();
     $(".tryelse").hide();
 
-    if (work === 'start') {
+// Java track done Done
+    if (work === 'start' || work === 'care' || work === 'mid') && ('enterprise') && (looks1 === 'no' || looks1 === 'yes') && (looks2 === 'no' || looks ='little') && (front === 'backend' || front === 'frontend')){
       $('.javadev').show()
-    }
 
+    else if ((work === 'mid' || work === 'care' || work === 'enterprise') && (looks1 === 'yes') && (looks2 === 'yes') && (front === 'frontend')){
+      $('.cssdev').show()
+
+    else if ((work === 'mid' || work === 'care' || work === 'enterprise') && (looks1 === 'no') && (looks2 === 'maybe') && (front === 'backend')){
+    $('.phpdev').show()
+
+    else if ((work === 'mid' || work === 'enterprise') && (looks1 === 'no') && (looks2 === 'no') && (front === 'backend' || front === 'frontend')){
+    $('.csharpdev').show()
 
   event.preventDefault();
   console.log(work)
+  console.log(looks1)
+  console.log(looks2)
+  console.log(front)
 
       });
       });
