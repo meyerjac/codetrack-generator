8a02ea2ab6de7467debea6f8844d0f5a41e046e2
Jackson Meyer <jackson.meyer7@gmail.com>
2016-08-12 12:56:02 -0700

    will add js to the hmtl and css for the first question

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,23 @@
 $(document).ready(function() {
-  prompt
-}
+  $(".input").submit(function(event) {
+    var work = $("#work").val();
+    var looks1 = $("#looks1").val();
+    var looks2 = $("#looks2").val();
+    var front = $("#front").val();
+
+    $(".cssdev").hide();
+    $(".phpdev").hide();
+    $(".javadev").hide();
+    $(".csharpdev").hide();
+    $(".tryelse").hide();
+
+    if (work === 'start') {
+      $('.javadev').show()
+    }
+
+
+  event.preventDefault();
+  console.log(work)
+
+      });
+      });
